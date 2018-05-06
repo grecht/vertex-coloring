17db9359443fd5d5d1da5395c50480e0561ab8d7
Gereon Recht <gereon.recht@protonmail.com>
2018-05-06 21:17:22 +0200

    Standalone version stripped-down for demonstration purposes.

---
+++ VertexColoringAlgorithm.java
@@ -0,0 +1,200 @@
+import java.util.*;
+
+/**
+ * A brute-force vertex coloring algorithm which determines all 
+ * non-equivalent colorings of a graph using a minimal amount of
+ * colors.
+ * 
+ * This algorithm was origininally developed for a class project 
+ * (https://github.com/DifferentLink/Grape). 
+ */ 
+public class VertexColoringAlgorithm {
+	private List<int[]> colorings;
+	private int[][] adjacencyMatrix;
+	private int largestCliqueSize;
+	
+	public VertexColoringAlgorithm(int[][] adjacencyMatrix) {
+		this.adjacencyMatrix = adjacencyMatrix;
+		this.largestCliqueSize = 0;
+		this.colorings = new ArrayList<>();
+	}
+	
+	public VertexColoringAlgorithm(int[][] adjacencyMatrix, int largestCliqueSize) {
+		this.adjacencyMatrix = adjacencyMatrix;
+		this.largestCliqueSize = largestCliqueSize;	
+		this.colorings = new ArrayList<>();
+	}
+	
+	public List<int[]> getAllColorings() {
+		int numberOfVertices = this.adjacencyMatrix[0].length;		
+		// trivial case
+		if (numberOfVertices == 1) {
+			this.colorings.add(new int[]{ 0 });
+			return this.colorings;
+		}
+		
+		int numberOfColors = Integer.MAX_VALUE;
+		int[] partition = getFirstPartition(numberOfVertices, largestCliqueSize);
+		int[] colors;
+		
+		// iterate over partitions
+		while (partition != null) {
+			if (partition.length > numberOfColors) {
+				// all colorings with have been found.
+				break;
+			}			
+			colors = this.partitionToColoring(partition, numberOfVertices);
+			do {
+				if (isValidVertexColoring(colors)) {
+					// found one valid coloring for this partition.
+					this.colorings.add(colors);
+					numberOfColors = partition.length;
+					break;
+				}
+			} while (getNextPermutation(colors));
+			partition = getNextPartition(partition, numberOfVertices);		
+		}	
+		return this.colorings;		
+	}
+	
+	// todo: simplify this.
+	private int[] partitionToColoring(int[] partition, int numberOfVertices) {
+		int[] result = new int[numberOfVertices];
+		int index = 0;
+		for (int i = 0; i < partition.length; i++) {
+			for (int j = index; j < result.length; j++) {
+				result[j] = i;
+			}
+			index += partition[i];
+		}
+		return result;
+	}
+	
+	// input array must be of ascending order
+	private boolean getNextPermutation(int[] coloring) {
+		int i = coloring.length - 1;
+		while (i > 0 && coloring[i - 1] >= coloring[i]) {
+			i--;
+		}
+		if (i <= 0) {
+			// last permutation
+			return false;
+		}
+		int j = coloring.length - 1;
+		while (coloring[j] <= coloring[i - 1]) {
+			j--;
+		}
+
+		// swap
+		int tmp = coloring[i - 1];
+		coloring[i - 1] = coloring[j];
+		coloring[j] = tmp;
+
+		j = coloring.length - 1;
+		while (i < j) {
+			tmp = coloring[i];
+			coloring[i] = coloring[j];
+			coloring[j] = tmp;
+			i++;
+			j--;
+		}
+		return true;
+	}
+	
+	private int[] getFirstPartition(int numberOfVertices, int largestCliqueSize) {
+		int[] result = new int[largestCliqueSize];
+		result[0] = numberOfVertices - (largestCliqueSize - 1);
+		for (int i = 1; i < result.length; i++) {
+			result[i] = 1;
+		}
+		return result;
+	}
+	
+	// todo: simplify this.
+	private int[] getNextPartition(int[] array, int numberOfVertices) {
+		if (array.length == numberOfVertices) {
+			return null;
+		}
+
+		int[] arrayCopy = new int[array.length];
+		System.arraycopy(array, 0, arrayCopy, 0, array.length);
+
+		// check if first element equals last element
+		// or if their difference is less than 2.
+		// this means that there are no more possible
+		// partitions for the numberOfVertices and
+		// numberOfColors in this iteration.
+		if (arrayCopy[0] == arrayCopy[arrayCopy.length - 1]
+				|| arrayCopy[0] - arrayCopy[arrayCopy.length - 1] < 2) {
+			// initialize arrayCopy with one more color.
+			arrayCopy = new int[arrayCopy.length + 1];
+			int tmp = numberOfVertices;
+			for (int j = arrayCopy.length - 1; j > 0; j--) {
+				arrayCopy[j] = 1;
+				tmp--;
+			}
+			arrayCopy[0] = tmp;
+			return arrayCopy;
+		}
+
+		// iterate over arrayCopy to calculate
+		// next distribution.
+		for (int j = 1; j < arrayCopy.length; j++) {
+			// check if the difference between
+			// the first and the j-th element
+			// of the arrayCopy is bigger than 2.
+			// this means that there exists
+			// a possible next distribution.
+			if (arrayCopy[0] - arrayCopy[j] >= 2) {
+				arrayCopy[j]++;
+				arrayCopy[j - 1]--;
+				j--;
+				// if arrayCopy is not in ascending order,
+				// the distribution is not correct yet.
+				while (!isInDescendingOrder(arrayCopy)) {
+					if (arrayCopy[j] >= arrayCopy[j - 1]) {
+						break;
+					}
+					arrayCopy[j]++;
+					arrayCopy[j - 1]--;
+					j--;
+				}
+				return arrayCopy;
+			}
+		}
+		return null;
+	}
+	
+	private boolean isInDescendingOrder(int[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			if (array[i] < array[i + 1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private boolean isValidVertexColoring(int[] colors) {
+		for (int i = 0; i < adjacencyMatrix.length; i++) {
+			for (int j = i + 1; j < adjacencyMatrix.length; j++) {
+				if (adjacencyMatrix[i][j] == 1 && colors[i] == colors[j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		int[][] adjacencyMatrix = new int[4][4];
+		adjacencyMatrix[0] = new int[]{ 1, 1, 1, 0 };
+		adjacencyMatrix[1] = new int[]{ 1, 1, 1, 0 };
+		adjacencyMatrix[2] = new int[]{ 1, 1, 1, 0 };
+		adjacencyMatrix[3] = new int[]{ 1, 0, 0, 0 };
+		VertexColoringAlgorithm alg = new VertexColoringAlgorithm(adjacencyMatrix, 3);
+		List<int[]> colorings = alg.getAllColorings();	
+		for (int[] coloring : colorings) {
+			System.out.println(Arrays.toString(coloring));
+		}
+	}
+}
